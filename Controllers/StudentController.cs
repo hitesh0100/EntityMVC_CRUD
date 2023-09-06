7a768fa5cbb4f5a8e8ac296ac15f381ec5021b29
Hitesh Patel <hitesh411999@gmail.com>
2023-09-06 13:34:38 -0400

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,99 @@
+﻿using EntityMVC_CRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityMVC_CRUD.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            //return View();
+            StudentDBHandle dbhandle = new StudentDBHandle();
+            ModelState.Clear();
+            return View(dbhandle.GetStudent());
+        }
+
+        // GET: Student/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        [HttpPost]
+        public ActionResult Create(Student smodel)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                if (ModelState.IsValid)
+                {
+                    StudentDBHandle sdb = new StudentDBHandle();
+                    if (sdb.AddStudent(smodel))
+                    {
+                        ViewBag.Message = "Student Details Added Successfully";
+                        ModelState.Clear();
+                    }
+                }
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int id)
+        {
+            StudentDBHandle sdb = new StudentDBHandle();
+            return View(sdb.GetStudent().Find(smodel => smodel.Id == id));
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Student smodel)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                StudentDBHandle sdb = new StudentDBHandle();
+                sdb.UpdateDetails(smodel);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                StudentDBHandle sdb = new StudentDBHandle();
+                if (sdb.DeleteStudent(id))
+                {
+                    ViewBag.AlertMsg = "Student Deleted Successfully";
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace EntityMVC_CRUD.Models
+{
+    public class Student
+    {
+            [Display(Name = "Id")]
+            public int Id { get; set; }
+
+            [Required(ErrorMessage = "First name is required.")]
+            public string Name { get; set; }
+
+            [Required(ErrorMessage = "City is required.")]
+            public string City { get; set; }
+
+            [Required(ErrorMessage = "Address is required.")]
+            public string Address { get; set; }      
+    }
+}
